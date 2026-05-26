e754d4c8ee0c603acf0d700366af0cb64949fedb
hero23550 <117738282+hero23550@users.noreply.github.com>
2026-05-26 10:18:15 +0530

    Create Home.jsx

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>PrimeTiers</h1>
+      <p>Competitive ranking system for players.</p>
+    </div>
+  )
+}
