910a6798aff537a5787d860ebf1424bb1228c472
hero23550 <117738282+hero23550@users.noreply.github.com>
2026-05-26 09:19:27 +0530

    Create App component with welcome message

---
+++ my-app/src/app.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "Arial"
+    }}>
+      <h1>Hello from Vercel 🚀</h1>
+    </div>
+  )
+}
