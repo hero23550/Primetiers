4402d4f9d5130762e62a031cd29cb8b3b475f945
hero23550 <117738282+hero23550@users.noreply.github.com>
2026-05-26 10:17:17 +0530

    Add player data to player.js

---
+++ my-app/src/data/player.js
@@ -0,0 +1,7 @@
+export const players = [
+  { id: "1", name: "Aspect", points: 980 },
+  { id: "2", name: "Aspect", points: 870 },
+  { id: "3", name: "Aspect", points: 760 },
+  { id: "4", name: "Aspect", points: 640 },
+  { id: "5", name: "Aspect", points: 520 }
+]
